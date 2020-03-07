fc04215cbe21bb2080ab770c613c57465c18c7ee
LuciaRivero <claudialucia.rivero@gmail.com>
2020-03-06 22:16:16 -0300

    modifico gameHelper

--- src/Components/Cell.js
+++ src/Components/Cell.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { StyledCell } from './Styles/StyledCell';
+import {TETROMINOS} from '../tetrominos';
 
 const Cell = ({type}) => (
-    <div>cell</div>
+    <StyledCell type={type} color={TETROMINOS[type]}>cell</StyledCell>
 )
 
 export default Cell;
--- src/Components/Tetris.js
+++ src/Components/Tetris.js
@@ -2,20 +2,25 @@ import React from 'react';
 import Stage from './Stage';
 import Display from './Display';
 import StartButton from './StartButton';
+import {createStage} from '../gameHelpers';
+import {StyledTetris} from './Styles/StyledTetris';
+import {StyledTetrisWrapper} from './Styles/StyledTetris';
 
 const Tetris = () => {
     return (
-        <div>
-            <Stage/>
-            <aside>
-                <div>
-                    <Display text="Score"/>
-                    <Display text="Rows"/>
-                    <Display text="Level"/>
-                </div>
-                <StartButton/>
-            </aside>
-        </div>
+        <StyledTetrisWrapper>
+            <StyledTetris>
+                <Stage stage={createStage()}/>
+                <aside>
+                    <div>
+                        <Display text="Score"/>
+                        <Display text="Rows"/>
+                        <Display text="Level"/>
+                    </div>
+                    <StartButton/>
+                </aside>
+            </StyledTetris>
+        </StyledTetrisWrapper>
     )
 }
 export default Tetris;
